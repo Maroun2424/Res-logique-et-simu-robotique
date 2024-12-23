47a309bb076db57a5125fa2421baaa8f3c3033be
gebrayel <maroun.gebrayel@etu.u-paris.fr>
2024-12-23 17:53:44 +0100

    Ajout de la méthode parse_args pour détecter les options de la commande

--- bin/interp.ml
+++ bin/interp.ml
@@ -31,6 +31,64 @@ let default_config = {
   prog_number = 1;
   print_code = false;                        (* Par défaut, pas d'affichage terminal *)
 }
+
+(* Analyse des arguments de la ligne de commande *)
+let parse_args () =
+  let args = Array.to_list Sys.argv |> List.tl in
+  let rec parse args config =
+    match args with
+    | [] -> config
+
+    (* Rectangle d'approximation *)
+    | "-abs" :: x_min :: y_min :: x_max :: y_max :: rest ->
+        let rect = {
+          x_min = float_of_string x_min;
+          y_min = float_of_string y_min;
+          x_max = float_of_string x_max;
+          y_max = float_of_string y_max;
+        } in
+        parse rest { config with abs_rectangle = Some rect }
+
+    (* Affichage des points*)
+    | "-cr" :: rest ->
+        parse rest { config with display_points = true }
+
+    (* Couleur de fond *)
+    | "-bc" :: r :: g :: b :: rest ->
+        let color = rgb (int_of_string r) (int_of_string g) (int_of_string b) in
+        parse rest { config with background_color = Some color }
+
+    (* Couleur de l'avant-plan (pas forcément utilisé) *)
+    | "-fc" :: r :: g :: b :: rest ->
+        let color = rgb (int_of_string r) (int_of_string g) (int_of_string b) in
+        parse rest { config with foreground_color = Some color }
+
+    (* Couleur du rectangle -abs *)
+    | "-rc" :: r :: g :: b :: rest ->
+        let color = rgb (int_of_string r) (int_of_string g) (int_of_string b) in
+        parse rest { config with rectangle_color = Some color }
+
+    (* Couleur du point/robot *)
+    | "-pc" :: r :: g :: b :: rest ->
+        let color = rgb (int_of_string r) (int_of_string g) (int_of_string b) in
+        parse rest { config with point_color = Some color }
+
+    (* Taille de la fenêtre *)
+    | "-size" :: w :: h :: rest ->
+        parse rest { config with width = int_of_string w; height = int_of_string h }
+
+    (*print -> on affiche le code dans le terminal à chaque pas *)
+    | "-print" :: rest ->
+        parse rest { config with print_code = true }
+
+    (* Choix du program *)
+    | prog :: rest when prog = "1" || prog = "2" || prog = "3" || prog = "4" ->
+        parse rest { config with prog_number = int_of_string prog }
+
+    (* Erreur : argument non reconnu *)
+    | arg :: _ -> failwith ("Argument inconnu ou mal forme : " ^ arg)
+  in
+  parse args default_config
 let () =
   try loop 5
   with Quit -> close_graph ();;
