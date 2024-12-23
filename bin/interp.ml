c16f0a66a429a5f938fba5ee4e234e52c90ec91d
gebrayel <maroun.gebrayel@etu.u-paris.fr>
2024-12-23 17:51:35 +0100

    type config pour la ligne de commande interp

--- bin/interp.ml
+++ bin/interp.ml
@@ -1,26 +1,36 @@
-open Graphics;;
+open Graphics
+open Pf5
+open Geo
+open Interp
+open Approx
 
-open_graph " 500x500";;
+(* Gestion des options en ligne de commande *)
+type config = {
+  abs_rectangle : rectangle option; (* option -abs Xmin Ymin Xmax Ymax *)
+  display_points : bool;             (* option -cr, affiche points *)
+  background_color : color option;   (* option -bc r g b *)
+  foreground_color : color option;  (* option -fc r g b *)
+  rectangle_color : color option;  (* option -rc r g b *)
+  point_color : color option;    (* option -pc r g b *)
+  width : int;                  (* option -size W H *)
+  height : int;
+  prog_number : int;                 (* programme 1,2,3.. *)
+  print_code : bool;                 (*option -print *)
+}
 
-exception Quit;;
-
-let rec loop t =
-  let eve = wait_next_event [Mouse_motion;Key_pressed]
-  in
-  if eve.keypressed
-  then 
-    match eve.key with
-      | 'b'  -> set_color black; loop t
-      | 'r'  -> set_color red; loop t
-      | 'g'  -> set_color green; loop t
-      | 'q'  -> raise Quit
-      | '0'..'9' as  x -> loop (int_of_string (String.make 1 x))
-      | _    -> loop t
-  else begin
-    fill_circle (eve.mouse_x-t/2) (eve.mouse_y-t/2) t;
-    loop t
-  end
-
+(* Configuration par défaut si aucune option n'est fourniee*)
+let default_config = {
+  abs_rectangle = None;
+  display_points = true;                      (* On affiche les points par défaut *)
+  background_color = Some (rgb 245 245 220); (* Beige par défaut *)
+  foreground_color = Some black;
+  rectangle_color = Some blue;
+  point_color = Some red;
+  width = 800;
+  height = 800;
+  prog_number = 1;
+  print_code = false;                        (* Par défaut, pas d'affichage terminal *)
+}
 let () =
   try loop 5
   with Quit -> close_graph ();;
